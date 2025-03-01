bf141616d631946419171d51f04ab3acfe3aada3
hani-momo <ogavamaria@gmail.com>
2025-03-01 10:17:08 +0300

    edit wordCount

--- wordCount.py
+++ wordCount.py
@@ -1,9 +1,9 @@
-from pyspark import SparkSession
+from pyspark.sql import SparkSession
 from pyspark.sql import functions as func
 
 spark = SparkSession.builder.appName("WordCount").getOrCreate()
 
-inputDF = spark.read.text("book.txt")
+inputDF = spark.read.text("./files/book.txt")
 words = inputDF.select(func.explode(func.split(inputDF.value, "\\W+")).alias("word"))
 wordsWithoutEmptyStrings = words.filter(words.word != "")
 
@@ -12,6 +12,7 @@ lowercaseWords = wordsWithoutEmptyStrings.select(func.lower(wordsWithoutEmptyStr
 wordCounts = lowercaseWords.groupBy("word").count() # count words' occurrences
 wordCountsSorted = wordCounts.sort("count")
 
-wordCountsSorted.show(wordCountsSorted.count())
+wordCountsSorted.show(50, truncate=False)
+#wordCountsSorted.show(wordCountsSorted.count())
 
 spark.stop()
